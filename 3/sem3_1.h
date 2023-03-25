d721302a77bf1dbaf03666a490c875319ca1264c
migalinas <121310733+migalinas@users.noreply.github.com>
2023-03-25 12:35:44 +0300

    Delete 3 directory

--- 3/sem3_1.h
+++
@@ -1,3 +0,0 @@
-#include <stdio.h>
-
-void removeChars(char s1[], char s2[]);
--- 3/sem3_3.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void count_digits(int digits_count[]);
-void print_horizontal_histogram(int digits_count[]);
-void print_vertical_histogram(int digits_count[]);
-
