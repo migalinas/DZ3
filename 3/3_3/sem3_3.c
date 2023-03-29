8f30e71a1f1411973977e0ac3c9041dd82b81660
migalinas <121310733+migalinas@users.noreply.github.com>
2023-03-29 21:54:27 +0300

    Update sem3_3.c

--- 3/3_3/sem3_3.c
+++ 3/3_3/sem3_3.c
@@ -3,14 +3,16 @@
 #include "sem3_3_func.c"
 
 int main() {
-    int digits_count[10] = {0};
-    count_digits(digits_count);
+    char input[1000];
+    printf("Enter some text (letters and digits only): ");
+    fgets(input, 1000, stdin);
 
-    printf("Horizontal histogram:\n");
-    print_horizontal_histogram(digits_count);
+    int num_digits = get_num_digits(input);
+    int frequencies[MAX_DIGITS] = {0};
+    fill_digit_frequencies(input, frequencies);
 
-    printf("\nVertical histogram:\n");
-    print_vertical_histogram(digits_count);
+    print_horizontal_histogram(frequencies);
+    print_vertical_histogram(frequencies);
 
     return 0;
 }
