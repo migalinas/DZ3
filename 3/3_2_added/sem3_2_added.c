25b8c6f9a685a1ab03f02011f6cb35e5f9b5ae82
migalinas <121310733+migalinas@users.noreply.github.com>
2023-03-30 00:38:34 +0300

    Update sem3_2_added.c

--- 3/3_2_added/sem3_2_added.c
+++ 3/3_2_added/sem3_2_added.c
@@ -4,12 +4,20 @@
 
 int main() {
     char s1[100], s2[100];
+    int i = 0;
 
     printf("Enter s1: ");
-    scanf("%s", s1);
+    while ((s1[i] = getchar()) != '\n' && i < 99) {
+        i++;
+    }
+    s1[i] = '\0';
 
+    i = 0;
     printf("Enter s2: ");
-    scanf("%s", s2);
+    while ((s2[i] = getchar()) != '\n' && i < 99) {
+        i++;
+    }
+    s2[i] = '\0';
 
     delete_string(s1, s2);
 
