5ae7462915776a1be82f074032dcc47827d29a67
migalinas <121310733+migalinas@users.noreply.github.com>
2023-03-29 19:14:17 +0300

    Update sem3_1_func.c

--- 3/3_1/sem3_1_func.c
+++ 3/3_1/sem3_1_func.c
@@ -1,16 +1,56 @@
+#include <stdio.h>
 #include "sem3_1.h"
 
-void removeChars(char s1[], char s2[]) {
-    int i, j, k;
-    for (i = 0; s1[i] != '\0'; i++) {
-        for (j = 0; s2[j] != '\0'; j++) {
-            if (s1[i] == s2[j]) {
-                for (k = i; s1[k] != '\0'; k++) {
-                    s1[k] = s1[k+1];
+string str_new() {
+    string s = { .len = 0 };
+    s.data[0] = '\0';
+    return s;
+}
+
+string str_read(char end) {
+    string s = str_new();
+    char c;
+    while ((c = getchar()) != end && c != EOF && s.len < STR_MAXLEN) {
+        s.data[s.len++] = c;
+    }
+    s.data[s.len] = '\0';
+    return s;
+}
+
+int charGroup(char c) {
+    if (c >= '!' && c <= '~') return 0;
+    return -1;
+}
+
+string str_fill_a_z(string old) {
+    string s = str_new();
+    int i = 0;
+    while (i < old.len) {
+        if (old.data[i] == old.data[i + 1] && old.data[i + 1] == old.data[i + 2]){
+            s.data[s.len++] = old.data[i++];
+        }
+        else {
+            if (charGroup(old.data[i]) > -1 &&
+                old.data[i+1] == '-' &&
+                charGroup(old.data[i]) == charGroup(old.data[i+2]) ) {
+            char z;
+            if (old.data[i] <= old.data[i+2]) {
+                for (z = old.data[i]; z <= old.data[i+2]; z++) {
+                    s.data[s.len++] = z;
                 }
-                i--;
-                break;
+            } else {
+                for (z = old.data[i]; z >= old.data[i+2]; z--) {
+                    s.data[s.len++] = z;
+                }
             }
+            i += 3;
+        } else if (old.data[i - 1] != '\0' && old.data[i + 1] != '\0' && old.data[i] == '-') {
+            s.data[s.len--] = old.data[i--];
+        } else {
+            s.data[s.len++] = old.data[i++];
+        }
         }
     }
+    s.data[s.len] = '\0';
+    return s;
 }
