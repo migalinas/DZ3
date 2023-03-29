7c60296654b2f7c15a46e679d9d4904d20e8f648
migalinas <121310733+migalinas@users.noreply.github.com>
2023-03-29 19:10:05 +0300

    Update sem3_1.c

--- 3/3_1/sem3_1.c
+++ 3/3_1/sem3_1.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 #include "sem3_1.h"
 #include "sem3_1_func.c"
+#define STR_MAXLEN 1000
+
+
 
 int main() {
-    char s1[100], s2[100];
-    int i;
-    printf("Enter s1: ");
-    i = 0;
-    while ((s1[i] = getchar()) != '\n') {
-        i++;
-    }
-    s1[i] = '\0';
-    printf("Enter s2: ");
-    i = 0;
-    while ((s2[i] = getchar()) != '\n') {
-        i++;
-    }
-    s2[i] = '\0';
-    removeChars(s1, s2);
-    printf("Result: %s\n", s1);
+    string s1 = str_read('\n');
+    string s2 = str_fill_a_z(s1);
+    printf("\"%s\"\n", s2.data);
     return 0;
 }
